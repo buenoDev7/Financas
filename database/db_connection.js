1e08846088e0c41bb0394b071df98ac288ca6fef
Yan Bueno <buenoy519@gmail.com>
2026-03-04 19:32:32 -0300

    >

--- database/db_connection.js
+++ database/db_connection.js
@@ -1,8 +1,13 @@
-const Sequelize = require('sequelize');
-const connection = new Sequelize('financas', 'root', '@Yan2004', {
-    host: 'localhost',
-    dialect: 'mysql',
-    timezone: '-03:00'
-});
+const connection = new Sequelize(
+    process.env.MYSQLDATABASE,
+    process.env.MYSQLUSER,
+    process.env.MYSQLPASSWORD,
+    {
+        host: process.env.MYSQLHOST,
+        port: process.env.MYSQLPORT,
+        dialect: 'mysql',
+        timezone: '-03:00'
+    }
+);
 
 module.exports = connection;
